176449c7eb2842d8dd05562b9397edd33dab5534
Hieu Phan <huyhieu.210198@gmail.com>
2019-06-22 14:11:43 -0400

    Add Gridsearch

--- titanic_nn.py
+++ titanic_nn.py
@@ -32,7 +32,7 @@ def read_data():
 
 
 # 3-layer NN
-def titanic_model(layer_1=128, layer_2=16):
+def titanic_model(layer_1=25, layer_2=35):
     model = Sequential()
     model.add(Dense(layer_1, input_shape=(5, )))
     model.add(Activation('relu'))
@@ -51,6 +51,7 @@ def titanic_model(layer_1=128, layer_2=16):
 
 x_train, y_train, x_test = read_data()
 
+# Grid search: number of hidden units in layer 1 and layer 2 
 layer_1 = [10, 15, 20, 25, 30, 35, 40]
 layer_2 = [10, 15, 20, 25, 30, 35, 40]
 param_grid = dict(layer_1=layer_1, layer_2=layer_2)
@@ -59,10 +60,12 @@ grid = GridSearchCV(estimator=keras_tt, param_grid=param_grid, cv=5, verbose=2)
 grid.fit(x_train, y_train)
 results = grid.cv_results_
 params = grid.best_params_
-print(params)
+print(params)       
+# {'layer_1': 25, 'layer_2': 35}
 
-"""
-tt_model = titanic_model()
+layer_1_units = params['layer_1']
+layer_2_units = params['layer_2']
+tt_model = titanic_model(layer_1=layer_1_units, layer_2=layer_2_units)
 tt_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 tt_model.fit(x_train, y_train, epochs=100, batch_size=16, verbose=1)
 test_prediction = tt_model.predict_classes(x_test)
@@ -72,4 +75,5 @@ with open('result.csv', mode='w') as result_file:
     writer.writerow(['PassengerId', 'Survived'])
     for p in range(892, 1310):
         writer.writerow([p, test_prediction[p - 892]])
-"""
+
+# Accuracy (Kaggle score): 0.77990
